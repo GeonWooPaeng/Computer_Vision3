5fd0ee8346fd61e469ccc3a9bbcf9b77a0b6f579
GeonWooPaeng <gwpaeng@naver.com>
2020-08-20 20:29:00 +0900

    외곽선 검출 & 그리기

--- Business_card_recognition_program/contours1.py
+++ Business_card_recognition_program/contours1.py
@@ -1,24 +1,30 @@
+# 외곽선 검출
+# 객체의 외곽선 좌표를 모두 추출하는 작업 
+# 바깥쪽 & 안쪽(홀) 외곽선 -> 외곽선의 계층 구조도 표현 가능
+
 import sys
 import random
 import numpy as np
 import cv2
 
 
-src = cv2.imread('contours.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\Business_card_recognition_program.\contours.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-contours, hier = cv2.findContours(src, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+#src: 이진영상
+contours, hier = cv2.findContours(src, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE) # 외곽선 검출 함수 
 
-dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR)
+dst = cv2.cvtColor(src, cv2.COLOR_GRAY2BGR) #출력영상을 color로 보여주기 위함
 
 idx = 0
 while idx >= 0:
+    #특정 외곽선을 그리기 위함
     c = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    cv2.drawContours(dst, contours, idx, c, 2, cv2.LINE_8, hier)
-    idx = hier[0, idx, 0]
+    cv2.drawContours(dst, contours, idx, c, 2, cv2.LINE_8, hier) # 외곽선을 화면에 그리기(검증), hier을 줘서 안쪽까지 나타낸다.
+    idx = hier[0, idx, 0] #hierarchy로 외곽선 계층 정보
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
--- Business_card_recognition_program/contours2.py
+++ Business_card_recognition_program/contours2.py
@@ -4,22 +4,27 @@ import numpy as np
 import cv2
 
 
-src = cv2.imread('milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
+src = cv2.imread('.\Business_card_recognition_program.\milkdrop.bmp', cv2.IMREAD_GRAYSCALE)
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
-_, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU)
+# _ 은 Otsu방법에의해 결정된 threshold값
+# src_bin 은 이진 영상
+_, src_bin = cv2.threshold(src, 0, 255, cv2.THRESH_OTSU) #이진화
 
-contours, _ = cv2.findContours(src_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+# _ 은 hierarchy
+# contours의 길이 = 검출한 외곽선의 개수
+contours, _ = cv2.findContours(src_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) #외곽선 검출
 
 h, w = src.shape[:2]
 dst = np.zeros((h, w, 3), np.uint8)
 
 for i in range(len(contours)):
+    #이어져 있는 외곽선은 같은 색이 나온다.
     c = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    cv2.drawContours(dst, contours, i, c, 1, cv2.LINE_AA)
+    cv2.drawContours(dst, contours, i, c, 1, cv2.LINE_AA) #외곽선을 보여주기 위함
 
 cv2.imshow('src', src)
 cv2.imshow('src_bin', src_bin)
