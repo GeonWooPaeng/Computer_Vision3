472a8084c6e77575ab40c2981d793989bc44498e
GeonWooPaeng <gwpaeng@naver.com>
2020-08-21 21:52:35 +0900

    name_card

--- Business_card_recognition_program/namecard.py
+++ Business_card_recognition_program/namecard.py
@@ -106,25 +106,3 @@ cv2.waitKey()
 cv2.destroyAllWindows()
 
 
-
-
-
-
-
-import sys 
-import random 
-import numpy as np 
-import cv2 
-import pytesseract 
-
-def reorderPts(pts):
-    idx = np.lexsort((pts[:,1], pts[:, 0]))
-    pts = pts[idx]
-
-    if pts[0,1] > pts[1,1]:
-        pts[[0,1]] = pts[[1,0]]
-
-    if pts[2,1] < pts[3,1]:
-        pts[[2,3]] = pts[[3,2]]
-
-    return pts
