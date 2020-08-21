ae212dd40f7696585e9f4b591c09c4c487e69974
GeonWooPaeng <gwpaeng@naver.com>
2020-08-21 11:12:43 +0900

    그랩컷(배경,객체 분할)

--- snow_app/grabcut1.py
+++ snow_app/grabcut1.py
@@ -1,26 +1,38 @@
+# 그랩컷 
+# - 그래프 컷 기반 영역 분할 알고리즘 (배경과 객체를 구분해준다.)
+# - 영상의 픽셀을 그래프 정점으로 간주하고, 
+# 픽셀들을 두 개의 그룹으로 나누는 최적의 컷(Max Flow Minimum Cut)을 찾는 방식
+
+#사용자 지정(ROI)으로 그랩컷 하는 것
+
 import sys
 import numpy as np
 import cv2
 
 
 # 입력 영상 불러오기
-src = cv2.imread('nemo.jpg')
+src = cv2.imread('.\snow_app.\\nemo.jpg')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
 
 # 사각형 지정을 통한 초기 분할
-rc = cv2.selectROI(src)
+rc = cv2.selectROI(src) #ROI 창이 뜬다(사각형 사용자 지정하기 위함)
+
 mask = np.zeros(src.shape[:2], np.uint8)
 
-cv2.grabCut(src, mask, rc, None, None, 5, cv2.GC_INIT_WITH_RECT)
+cv2.grabCut(src, mask, rc, None, None, 5, cv2.GC_INIT_WITH_RECT) #그랩컷 하는 부분 (mask 계속 update한다.)
 
-# 0: cv2.GC_BGD, 2: cv2.GC_PR_BGD
+# 0: cv2.GC_BGD(), 2: cv2.GC_PR_BGD() -> background 인것 or 인것같은 것
+# 1: cv2.GC_FGD(), 3: cv2.GC_PR_FGD() -> 객체 인것 or 인것 같은것
 mask2 = np.where((mask == 0) | (mask == 2), 0, 1).astype('uint8')
 dst = src * mask2[:, :, np.newaxis]
 
+mask = mask * 64
+
 # 초기 분할 결과 출력
 cv2.imshow('dst', dst)
+cv2.imshow('mask',mask)
 cv2.waitKey()
 cv2.destroyAllWindows()
--- snow_app/grabcut2.py
+++ snow_app/grabcut2.py
@@ -4,11 +4,13 @@ import cv2
 
 
 # 입력 영상 불러오기
-src = cv2.imread('messi5.jpg')
+src = cv2.imread('.\snow_app.\grapcut_img.jpg')
 
 if src is None:
     print('Image load failed!')
     sys.exit()
+
+src = cv2.resize(src, dsize=(640,480), interpolation=cv2.INTER_AREA)
 
 # 사각형 지정을 통한 초기 분할
 mask = np.zeros(src.shape[:2], np.uint8)  # 마스크
@@ -19,7 +21,9 @@ rc = cv2.selectROI(src)
 
 cv2.grabCut(src, mask, rc, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_RECT)
 
-# 0: cv2.GC_BGD, 2: cv2.GC_PR_BGD
+
+# 0: cv2.GC_BGD(), 2: cv2.GC_PR_BGD() -> background 인것 or 인것같은 것
+# 1: cv2.GC_FGD(), 3: cv2.GC_PR_FGD() -> 객체 인것 or 인것 같은것
 mask2 = np.where((mask == 0) | (mask == 2), 0, 1).astype('uint8')
 dst = src * mask2[:, :, np.newaxis]
 
@@ -29,19 +33,24 @@ cv2.imshow('dst', dst)
 # 마우스 이벤트 처리 함수 등록
 def on_mouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
+        # 왼쪽 버튼(객체)
         cv2.circle(dst, (x, y), 3, (255, 0, 0), -1)
         cv2.circle(mask, (x, y), 3, cv2.GC_FGD, -1)
         cv2.imshow('dst', dst)
     elif event == cv2.EVENT_RBUTTONDOWN:
+        # 오른쪽 버튼(배경)
         cv2.circle(dst, (x, y), 3, (0, 0, 255), -1)
         cv2.circle(mask, (x, y), 3, cv2.GC_BGD, -1)
         cv2.imshow('dst', dst)
     elif event == cv2.EVENT_MOUSEMOVE:
+        #마우스 눌린 상태
         if flags & cv2.EVENT_FLAG_LBUTTON:
+            # 왼쪽 버튼(객체)
             cv2.circle(dst, (x, y), 3, (255, 0, 0), -1)
             cv2.circle(mask, (x, y), 3, cv2.GC_FGD, -1)
             cv2.imshow('dst', dst)
         elif flags & cv2.EVENT_FLAG_RBUTTON:
+            # 오른쪽 버튼(배경)
             cv2.circle(dst, (x, y), 3, (0, 0, 255), -1)
             cv2.circle(mask, (x, y), 3, cv2.GC_BGD, -1)
             cv2.imshow('dst', dst)
@@ -53,6 +62,7 @@ while True:
     key = cv2.waitKey()
     if key == 13:  # ENTER
         # 사용자가 지정한 전경/배경 정보를 활용하여 영상 분할
+        # 그랩컷 다시 호출
         cv2.grabCut(src, mask, rc, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_MASK)
         mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
         dst = src * mask2[:, :, np.newaxis]
