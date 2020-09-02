7d8abe3bb4565f7e43fc0a3c2c1d4bc35086ecb9
GeonWooPaeng <gwpaeng@naver.com>
2020-09-03 00:02:37 +0900

    호모그래피(사진 매칭)

--- AR_video_player/homography.py
+++ AR_video_player/homography.py
@@ -1,13 +1,17 @@
+# 호모그래피
+# 두 평면 사이의 투시 변환(3x3)
+# 원래 사진과 비스듬한 사진을 비교한 것
+
 import sys
 import numpy as np
 import cv2
 
 
 # 영상 불러오기
-src1 = cv2.imread('graf1.png', cv2.IMREAD_GRAYSCALE)
-src2 = cv2.imread('graf3.png', cv2.IMREAD_GRAYSCALE)
-#src1 = cv2.imread('box.png', cv2.IMREAD_GRAYSCALE)
-#src2 = cv2.imread('box_in_scene.png', cv2.IMREAD_GRAYSCALE)
+# src1 = cv2.imread('.\AR_video_player.\graf1.png', cv2.IMREAD_GRAYSCALE)
+# src2 = cv2.imread('.\AR_video_player.\graf3.png', cv2.IMREAD_GRAYSCALE)
+src1 = cv2.imread('.\AR_video_player.\\box.png', cv2.IMREAD_GRAYSCALE)
+src2 = cv2.imread('.\AR_video_player.\\box_in_scene.png', cv2.IMREAD_GRAYSCALE)
 
 if src1 is None or src2 is None:
     print('Image load failed!')
@@ -27,7 +31,7 @@ matcher = cv2.BFMatcher_create()
 #matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING)
 matches = matcher.match(desc1, desc2)
 
-# 좋은 매칭 결과 선별
+# 좋은 매칭 결과 선별(상위 80개만 사용)
 matches = sorted(matches, key=lambda x: x.distance)
 good_matches = matches[:80]
 
@@ -37,22 +41,26 @@ print('# of matches:', len(matches))
 print('# of good_matches:', len(good_matches))
 
 # 호모그래피 계산
+# queryIdx: 1번 img에서의 keypoint 번호 / .pt 점의 좌표
 pts1 = np.array([kp1[m.queryIdx].pt for m in good_matches]
                 ).reshape(-1, 1, 2).astype(np.float32)
+
+# trainIdx: 2번 img에서의 keypoint 번호 / .pt 점의 좌표 
 pts2 = np.array([kp2[m.trainIdx].pt for m in good_matches]
                 ).reshape(-1, 1, 2).astype(np.float32)
 
-H, _ = cv2.findHomography(pts1, pts2, cv2.RANSAC)
+# H에는 두 영상관의 perspective 관계를 가지고 있다.
+H, _ = cv2.findHomography(pts1, pts2, cv2.RANSAC) #호모그래피 계산 함수
 
-# 호모그래피를 이용하여 기준 영상 영역 표시
+# 호모그래피를 이용하여 기준 영상 영역 표시(박스로 표현)
 dst = cv2.drawMatches(src1, kp1, src2, kp2, good_matches, None,
                       flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
 (h, w) = src1.shape[:2]
 corners1 = np.array([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]
-                    ).reshape(-1, 1, 2).astype(np.float32)
-corners2 = cv2.perspectiveTransform(corners1, H)
-corners2 = corners2 + np.float32([w, 0])
+                    ).reshape(-1, 1, 2).astype(np.float32) #입력 영상
+corners2 = cv2.perspectiveTransform(corners1, H) # 점을 가지고 투시변환 행렬(H)을 가지고 이동 시키는 것
+corners2 = corners2 + np.float32([w, 0]) # shift 해주는 곳
 
 cv2.polylines(dst, [np.int32(corners2)], True, (0, 255, 0), 2, cv2.LINE_AA)
 
