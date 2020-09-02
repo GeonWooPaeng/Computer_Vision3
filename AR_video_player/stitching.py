b1454180042348eff543b9bc85a51cae1cfbcc82
GeonWooPaeng <gwpaeng@naver.com>
2020-09-03 00:11:30 +0900

    이미지 스티칭(사진 연결)

--- AR_video_player/stitching.py
+++ AR_video_player/stitching.py
@@ -1,9 +1,16 @@
+# 이미지 스티칭
+# 여러장의 사진을 자연스럽게 붙여서 한장의 사진으로 만드는 기술 
+# 사진 이어 붙이기, 파노라마 영상
+
+# 방법
+# 여러장의 영상에서 특징점을 검출하고 특징점이 겹치는 것을 찾아서 두 영상의 perspective부분을 찾아서 이어 붙인다.
+
 import sys
 import numpy as np
 import cv2
 
 
-img_names = ['img1.jpg', 'img2.jpg', 'img3.jpg']
+img_names = ['.\AR_video_player.\img1.jpg', '.\AR_video_player.\img2.jpg', '.\AR_video_player.\img3.jpg']
 
 imgs = []
 for name in img_names:
@@ -15,14 +22,14 @@ for name in img_names:
 
     imgs.append(img)
 
-stitcher = cv2.Stitcher_create()
-status, dst = stitcher.stitch(imgs)
+stitcher = cv2.Stitcher_create() # 이미지 스티칭 객체 생성
+status, dst = stitcher.stitch(imgs) # 이미지 스티칭 함수(영상 합성)
 
-if status != cv2.Stitcher_OK:
+if status != cv2.Stitcher_OK: 
     print('Stitch failed!')
     sys.exit()
 
-cv2.imwrite('output.jpg', dst)
+cv2.imwrite('output.jpg', dst) # 결과 저장
 
 cv2.namedWindow('dst', cv2.WINDOW_NORMAL)
 cv2.imshow('dst', dst)
